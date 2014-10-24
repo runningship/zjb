f0171cf937a7794c37f89691355a7023130246b6
runningship <runningship@gmail.com>
2014-10-24 18:01:30 +0800

    更新版本号

--- src/main/java/com/youwei/zjb/view/home.java
+++ src/main/java/com/youwei/zjb/view/home.java
@@ -25,7 +25,7 @@ public class home {
 		html = html.replace("$${role}", role==null?"":role.title);
 		html = html.replace("$${tel}", u.tel==null? "" : u.tel);
 		
-		html = html.replace("$${version}", "5.0.108");
+		html = html.replace("$${version}", "5.0.1024");
 		return Jsoup.parse(html);
 	}
 }
