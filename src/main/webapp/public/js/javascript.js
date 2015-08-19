c647b0e07fc59cd4f74bfe3204a9dcf8a64207fe
runningship <runningship@gmail.com>
2015-08-19 14:31:13 +0800

    修改上传

--- src/main/webapp/public/js/javascript.js
+++ src/main/webapp/public/js/javascript.js
@@ -268,8 +268,19 @@ $(document).on('click', '.btn_act', function(event) {
         });
     }else if(ThiType=='seeMyHouse'){
         layer.msg('查看我的房源')
-
+    }else if(ThiType=='releaseHouse'){
+        layer.closeAll();
+        layer.open({
+            type: 1,
+            title: false,
+            closeBtn: false,
+            shadeClose: true,
+            area: ['auto', 'auto'],
+            skin: 'releaseHouse',
+            content: '<a href="#" class="s btn btn_act" data-type="addHouse">发布出售</a><a href="#" class="z btn btn_act" data-type="addRent">发布出租</a>'
+        });
     }else if(ThiType=='addHouse'){
+        layer.closeAll();
         layer.open({
             type: 2,
             title:'添加房源',
@@ -279,6 +290,7 @@ $(document).on('click', '.btn_act', function(event) {
             content: 'chushou_add.jsp'
         });
     }else if(ThiType=='editHouse'){
+        layer.closeAll();
         var hid=$(this).parents('tr').data('hid');
         layer.open({
             type: 2,
@@ -289,6 +301,7 @@ $(document).on('click', '.btn_act', function(event) {
             content: 'chushou_edit.jsp?hid='+hid
         });
     }else if(ThiType=='delHouse'){
+        layer.closeAll();
         var tr=$(this).parents('tr'),hid=tr.data('hid');
         layer.confirm('确定删除房源？', {icon: 3,
             btn: ['删除','取消'], //按钮
@@ -298,6 +311,7 @@ $(document).on('click', '.btn_act', function(event) {
             layer.msg('已删除', {icon: 1});
         }, function(){});
     }else if(ThiType=='addRent'){
+        layer.closeAll();
         layer.open({
             type: 2,
             title:'添加租房',
@@ -307,6 +321,7 @@ $(document).on('click', '.btn_act', function(event) {
             content: 'chuzu_add.jsp'
         });
     }else if(ThiType=='editRent'){
+        layer.closeAll();
         var hid=$(this).parents('tr').data('hid');
         layer.open({
             type: 2,
@@ -317,6 +332,7 @@ $(document).on('click', '.btn_act', function(event) {
             content: 'chuzu_edit.jsp?hid='+hid
         });
     }else if(ThiType=='delRent'){
+        layer.closeAll();
         layer.confirm('确定删除租房？', {icon: 3,
             btn: ['删除','取消'], //按钮
             shade: false //不显示遮罩
