a01b21b3fa68acd10b81d4435e1397646e84ad84
xzye <runningship@gmail.com>
2016-03-19 09:26:02 +0800

    修改老房源的检查重复

--- src/main/java/com/youwei/zjb/house/HouseService.java
+++ src/main/java/com/youwei/zjb/house/HouseService.java
@@ -214,7 +214,9 @@ public class HouseService {
 		ModelAndView mv = new ModelAndView();
 		ModelAndView result = exist(house.area , house.dhao , house.fhao , house.seeGX==null ? "0": house.seeGX.toString());
 		if("1".equals(result.data.getString("exist"))){
-			throw new GException(PlatformExceptionType.BusinessException,"存在相同的房源"+result.data.get("hid"));
+			if(house.id == result.data.getInt("hid")){
+				throw new GException(PlatformExceptionType.BusinessException,"存在相同的房源"+result.data.get("hid"));
+			}
 		}
 		House po = dao.get(House.class, house.id);
 		po.area = house.area;
