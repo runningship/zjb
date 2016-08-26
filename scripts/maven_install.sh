6190509ad39a41e58ac586e3bcdb9baad261a902
alxu <alxu@cisco.com>
2016-08-26 16:05:20 +0800

    update pom xml

--- scripts/maven_install.sh
+++ scripts/maven_install.sh
@@ -3,6 +3,7 @@ LIB=../lib
 
 mvn install:install-file -DgroupId=com.microsoft -DartifactId=sqljdbc4 -Dversion=4.0 -Dpackaging=jar -Dfile=$LIB/sqljdbc4-4.0.jar
 mvn install:install-file -DgroupId=com.cloopen -DartifactId=CCPRestSDK -Dversion=2.6 -Dpackaging=jar -Dfile=$LIB/CCPRestSDK-2.6.jar 
+mvn install:install-file -DgroupId=org.java-websocket -DartifactId=bc-Java-WebSocket -Dversion=0.1 -Dpackaging=jar -Dfile=$LIB/bc-Java-WebSocket-0.1.jar
 
 
 
